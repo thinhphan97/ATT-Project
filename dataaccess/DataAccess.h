1af1d35d87f9897218c9f1c0d6eccd48f2a70b21
THINH VAN PHAN/LGEVH VS FUNCTIONAL TECHNOLOGY 4(thinh.phan@lge.com) <thinh.phan@lge.com>
2020-12-30 11:47:59 +0700

    add file virtual Data access

--- dataaccess/DataAccess.h
+++ dataaccess/DataAccess.h
@@ -1,1 +1,14 @@
-#ifndef Project_dataaccess
+#ifndef Project_dataaccess_DataAccess_h_
+#define Project_dataaccess_DataAccess_h_
+
+class DataAccess
+{
+public:
+    virtual int AddData()=0;
+    virtual int DeleteData()=0;
+    virtual int UpdateData()=0;
+    virtual int SelectAllData()=0;
+    virtual int SelectData()=0;
+
+};
+#endif
